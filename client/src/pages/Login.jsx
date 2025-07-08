6eb24ab25cf9b900459c7866af5297c306778fca
krajan2006 <kushawahaharikishan1@gmail.com>
2025-07-08 11:31:02 +0530

    login setup done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import { AppWindowIcon, CodeIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
-function Login() {
+const Login = () => {
+  
+
   return (
-    <div>Login</div>
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Signup</CardTitle>
+              <CardDescription>
+                Create a new account and click signup when you're done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-name">Name</Label>
+                <Input type="text" placeholder="Enter your name" required="true" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-username">Email</Label>
+                <Input type="email" placeholder="Enter your email"  required="true" />
+              </div>
+               <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-username">Password</Label>
+                <Input type="password" placeholder="Enter your password"  required="true" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Signup</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>
+                Login your password here. After signup, you'll be logged in.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-current">Email</Label>
+                <Input type="email" placeholder="Enter your email"  required="true" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-new">Password</Label>
+                <Input type="password" placeholder="Enter your password"  required="true" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+    
   )
 }
 
