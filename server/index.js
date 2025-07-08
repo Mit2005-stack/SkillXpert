074a98f19d26e5d7d4aca5cd9593cfe3c703389a
krajan2006 <kushawahaharikishan1@gmail.com>
2025-07-08 17:32:59 +0530

    login backend complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoute from './route/user.route.js';
-
+import cookieParser from 'cookie-parser';
 import connectDB from './database/db.js';
+import cors from 'cors';
+
 dotenv.config({});
 
 connectDB();
@@ -10,15 +12,23 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:8080",
+  credentials: true,
+}));
+
 //apis
 app.use("/api/v1/user", userRoute);
 
-// app.get("/home", (_, res) => {
-//   res.status(200).json({
-//     success: true,
-//     message: "BrightPath API is running",
-//   });
-// });
+app.get("/home", (_, res) => {
+  res.status(200).json({
+    success: true,
+    message: "BrightPath API is running",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
