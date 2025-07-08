b84df63771bbe5476914d77349e6c576bd604deb
krajan2006 <kushawahaharikishan1@gmail.com>
2025-07-08 15:35:58 +0530

    mongo db connection initiated

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './database/db.js';
+dotenv.config({});
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+})
