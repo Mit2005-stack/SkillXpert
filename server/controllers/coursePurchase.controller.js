a60851e4aec51b94c8b585756e9d80ca809ec76b
Mit2005-stack <mitdumaniya05@gmail.com>
2025-07-20 12:06:41 +0530

    stripes setup improved.

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -1,6 +1,7 @@
 import Stripe from "stripe"
 import {Course} from "../model/course.model.js"
 import {CoursePurchase} from "../model/coursePurchase.model.js"
+import { Lecture } from "../model/lecture.model.js";
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -22,9 +23,96 @@ export const createCheckoutSession = async(req,res)=>{
         });
 
         //Create a strip checkout session
-        
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'inr',
+                        product_data: {
+                            name: course.courseTitle,
+                            images: [course.courseThumbnail],
+                        },
+                        unit_amount: course.coursePrice * 100, // Amount in paise
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: `${process.env.FRONTEND_URL}/course-progress/${courseId}`,
+            cancel_url: `${process.env.FRONTEND_URL}/course-details/${courseId}`,
+            metadata: {
+                courseId: courseId,
+                userId: userId,
+            },
+            shipping_address_collection: {
+                allowed_countries: ['IN'],
+            },
+        });
+        if(!session.url){
+            return res.status(400).json({success:false,message:"Error while creating session"});
+        }
+
+        //save the purchase record
+        newPurchase.paymentId = session.id;
+        await newPurchase.save();
+
+        return res.status(200).json({
+            success: true,
+            url: session.url,
+        });
 
     } catch (error) {
         console.log(error)
     }
-}
+};
+export const webhook = async (req, res) => {
+    let event;
+    try {
+        const payloadString = JSON.stringify(req.body, null, 2);
+        const secure_url = process.env.WEBHOOK_ENDPOINT_SECRET;
+
+        const header = stripe.webhooks.generateTestHeaderString({
+            payload: payloadString,
+            secret,
+        });
+        event = stripe.webhooks.constructEvent(payloadString, header, secret);
+    } catch (error) {
+        console.error("Webhook Error:", error.message);
+        return res.status(400).send(`Webhook Error: ${error.message}`);
+    }
+    // Handle the event
+    if (event.type === 'checkout.session.completed') {
+        try {
+            const session = event.data.object;
+            const purchase = await CoursePurchase.findOne({ paymentId: session.id }).populate({ path: 'courseId' });
+
+            if(!purchase){
+                return res.status(404).json({ message: "Purchase not found" });
+            }
+            if(session.amount_total){
+                purchase.amount = session.amount_total / 100; // Convert from paise to rupees
+            }
+            purchase.status = "completed";
+            //Make all lectures visible by setting `isPreviewFree` to true
+            if(purchase.courseId &&purchase.courseId.lectures.length > 0) {
+                await Lecture.updateMany(
+                    { _id: { $in: purchase.courseId.lectures } },
+                    { $set: { isPreviewFree : true } }
+                );
+            }
+            await purchase.save();
+            // Update user's enrolled courses
+            await Course.findByIdAndUpdate(
+                purchase.courseId._id,
+                { $addToSet: { enrolledcourses : purchase.userId } },//Add userid to enrolled courses
+                { new: true }
+            );
+        } catch (error) {
+            console.error("Error handling event:", error);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    }
+    res.status(200).send();
+
+};
